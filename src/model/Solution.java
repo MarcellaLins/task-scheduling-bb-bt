587668eaf3fb080bac7dbe15404ca75e5c617fce
Marcella Viana <vianamarcella05@gmail.com>
2026-03-09 07:24:13 -0300

    add Solution class with properties and constructor

--- src/model/Solution.java
+++ src/model/Solution.java
@@ -1,4 +1,25 @@
 package model;
+import java.util.List;
 
 public class Solution {
+
+    private List<Task> tasks;
+    private int cost;
+
+    public Solution(List<Task> tasks, int cost) {
+        this.tasks = tasks;
+        this.cost = cost;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
 }
