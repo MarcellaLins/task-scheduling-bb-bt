75516712783a06dc527b895879436c400fac7df4
Marcella Viana <vianamarcella05@gmail.com>
2026-03-09 07:21:25 -0300

    add Task class with properties and constructor

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Task {
+
+    private int id;
+    private int processingTime;
+    private int deadline;
+
+    public Task(int id, int processingTime, int deadline) {
+        this.id = id;
+        this.processingTime = processingTime;
+        this.deadline = deadline;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProcessingTime() {
+        return processingTime;
+    }
+
+    public int getDeadline() {
+        return deadline;
+    }
+
 }
