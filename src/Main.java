cd6e8441184f7263db43b74572be7980d4e71d36
Marcella Viana <vianamarcella05@gmail.com>
2026-03-09 23:15:10 -0300

    add input generation and reading functionality in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import utils.InputGenerator;
+import utils.InputReader;
+import model.Task;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // gera arquivos de entrada
+        InputGenerator.generate("input5.txt", 5);
+        InputGenerator.generate("input10.txt", 10);
+        InputGenerator.generate("input15.txt", 15);
+        InputGenerator.generate("input20.txt", 20);
+        InputGenerator.generate("input25.txt", 25);
+
+        // lê uma instância para testar
+        List<Task> tasks = InputReader.read("input10.txt");
+
+        // imprime as tarefas lidas
+        System.out.println("Tarefas lidas do arquivo:");
+
+        for (Task task : tasks) {
+            System.out.println(task);
         }
     }
 }
